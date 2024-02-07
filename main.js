68f4e095775a34637544f111bdad63e2554b8516
Ting-Hsuan Huang <felixhuang07@gmail.com>
2024-02-08 00:30:25 +0800

    Done with UI, need to add items

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-let totalClick = localStorage.getItem('click-count') || 0n;
-let totalMoney = localStorage.getItem("money-count") || 0n;
+let totalClick = BigInt(localStorage.getItem("click-count") || 0n);
+let totalMoney = BigInt(localStorage.getItem("money-count") || 0n);
+let moneyPerClick = BigInt(localStorage.getItem("money-per-click") || 1n);
+let moneyPerSecond = BigInt(localStorage.getItem("money-per-second") || 0n);
 
 window.onload = () => {
     const clickCounter = document.getElementById("click-count");
     clickCounter.textContent = `Total Click: ${totalClick}`;
 
     const moneyCounter = document.getElementById("money-count");
-    moneyCounter.textContent = `${totalMoney}$`
+    moneyCounter.textContent = `$${totalMoney}`
+
+    const mpc = document.getElementById("money-per-click");
+    mpc.textContent = `$${moneyPerClick} / Click`;
+
+    const mps = document.getElementById("money-per-second");
+    mps.textContent = `$${moneyPerSecond} / Second`;
 };
 
 const clickCounter = document.getElementById("click-count");
@@ -15,9 +23,15 @@ const mainButton = document.getElementById("main-button");
 
 mainButton.onclick = () => {
     totalClick++;
-    totalMoney++;
+    totalMoney += moneyPerClick;
     clickCounter.textContent = `Total Click: ${totalClick}`;
-    moneyCounter.textContent = `${totalMoney}$`;
-    localStorage.setItem('click-count', totalClick);
-    localStorage.setItem('money-count', totalMoney);
+    moneyCounter.textContent = `$${totalMoney}`;
+    localStorage.setItem("click-count", totalClick);
+    localStorage.setItem("money-count", totalMoney);
 };
+
+setInterval(() => {
+    totalMoney += moneyPerSecond;
+    moneyCounter.textContent = `$${totalMoney}`;
+    localStorage.setItem("money-count", totalMoney);
+}, 500);
