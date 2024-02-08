a7e6a00817c60c61a205d9c92de6e447c7c85d66
Ting-Hsuan Huang <felixhuang07@gmail.com>
2024-02-08 22:45:44 +0800

    All done

--- main.js
+++ main.js
@@ -3,35 +3,74 @@ let totalMoney = BigInt(localStorage.getItem("money-count") || 0n);
 let moneyPerClick = BigInt(localStorage.getItem("money-per-click") || 1n);
 let moneyPerSecond = BigInt(localStorage.getItem("money-per-second") || 0n);
 
-window.onload = () => {
-    const clickCounter = document.getElementById("click-count");
+const clickCounter = document.getElementById("click-count");
+const moneyCounter = document.getElementById("money-count");
+const mpc = document.getElementById("money-per-click");
+const mps = document.getElementById("money-per-second");
+
+const updateScreen = () => {
     clickCounter.textContent = `Total Click: ${totalClick}`;
-
-    const moneyCounter = document.getElementById("money-count");
     moneyCounter.textContent = `$${totalMoney}`
-
-    const mpc = document.getElementById("money-per-click");
     mpc.textContent = `$${moneyPerClick} / Click`;
-
-    const mps = document.getElementById("money-per-second");
     mps.textContent = `$${moneyPerSecond} / Second`;
 };
 
-const clickCounter = document.getElementById("click-count");
-const moneyCounter = document.getElementById("money-count");
+window.onload = updateScreen;
+
+const restartButton = document.getElementById("restart-button");
+
+restartButton.onclick = () => {
+    totalClick = 0n;
+    totalMoney = 0n;
+    moneyPerClick = 1n;
+    moneyPerSecond = 0n;
+    localStorage.clear();
+    localStorage.setItem("money-per-click", 1n);
+    updateScreen();
+};
+
 const mainButton = document.getElementById("main-button");
 
 mainButton.onclick = () => {
     totalClick++;
     totalMoney += moneyPerClick;
-    clickCounter.textContent = `Total Click: ${totalClick}`;
-    moneyCounter.textContent = `$${totalMoney}`;
     localStorage.setItem("click-count", totalClick);
     localStorage.setItem("money-count", totalMoney);
+    updateScreen();
 };
 
 setInterval(() => {
     totalMoney += moneyPerSecond;
-    moneyCounter.textContent = `$${totalMoney}`;
     localStorage.setItem("money-count", totalMoney);
-}, 500);
+    updateScreen();
+}, 1000);
+
+let items = document.getElementById("item-container").querySelectorAll(".item");
+
+items.forEach((e) => {
+    console.log(e);
+    e.onclick = () => {
+        let cost = 0n;
+        for(let i = 0; i < e.children.length; i++) {
+            if(e.children[i].className == "buy-button") {
+                cost = BigInt(e.children[i].textContent.substr(1));
+                break;
+            }
+        }
+        if(cost > totalMoney) {
+            return;
+        }
+        totalMoney -= cost;
+        localStorage.setItem("money-count", totalMoney);
+        let content = e.id;
+        let increment = BigInt(content.substr(0, content.length - 3));
+        if(content[content.length - 1] == 'c') {
+            moneyPerClick += increment;
+            localStorage.setItem("money-per-click", moneyPerClick);
+        } else {
+            moneyPerSecond += increment;
+            localStorage.setItem("money-per-second", moneyPerSecond);
+        }
+        updateScreen();
+    };
+});
